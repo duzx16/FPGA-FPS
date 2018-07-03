ef6a03e54892a86d228b3ff0f044f9ade4154d1f
Root Lee <ligen_thu@163.com>
2018-07-03 10:00:56 +0800

    添加注释

--- vga_calc.vhd
+++ vga_calc.vhd
@@ -36,6 +36,7 @@ entity vga_calc is
         object_statuses: in object_status_array;
         object_values: in object_value_array;
         
+	--sram
         base_sram_we, base_sram_oe, base_sram_ce : out std_logic;
         base_sram_addr : out std_logic_vector(19 downto 0);
         base_sram_data : inout std_logic_vector(31 downto 0);
@@ -73,15 +74,17 @@ architecture bhv of vga_calc is
         );
     end component;
     
-    signal s_x:std_logic_vector(9 downto 0);
+    signal s_x:std_logic_vector(9 downto 0);  --扫描位置的临时变量
     signal s_y:std_logic_vector(8 downto 0);
     signal clk50:std_logic;
-    signal q_vga:std_LOGIC_vector(9 downto 0);
+    signal q_vga:std_LOGIC_vector(9 downto 0);  --给vga赋值，与外界接口连接
     
+    --opencv处理得到图像边缘
     type GUN_PIXEL_LIMIT is array(0 to 2 * HGUN_HEIGHT - 1) of integer range 0 to 2 * HGUN_WIDTH;
     type ME_PIXEL_LIMIT is array(0 to 159) of integer range 0 to 160;
     type ENEMY_PIXEL_LIMIT is array(0 to 2 * HENEMY_HEIGHT - 1) of integer range 0 to 2 * HENEMY_WIDTH;
 
+    --代表不规则图形的左边缘及右边缘。如果中间有掏空的部分，则左边缘有两个。
     constant enemy_no_pixel_left1: ENEMY_PIXEL_LIMIT := (14,12,11,9,9,9,9,9,9,9,10,10,10,9,11,8,8,8,7,7,6,6,5,5,5,4,3,3,2,2,2,1,2,3,4,14,14,14,14,15,15,15,15,15,15,15,15,15,15,14,13,11,10,9,8,7,6,6,5,4,4,3,3,2,2,1,1,1,1,1,1,1,1,2,3,30,30,31,32,34);
     constant enemy_no_pixel_right1: ENEMY_PIXEL_LIMIT := (19,20,21,22,23,24,24,25,25,26,26,26,26,29,36,36,36,36,42,41,41,41,42,43,43,43,44,43,43,42,42,43,9,9,7,45,45,39,37,37,37,37,37,37,39,40,41,41,42,42,42,42,41,41,41,40,40,41,41,41,40,41,41,41,41,24,21,19,18,16,14,11,9,7,4,41,40,39,38,37);
     constant enemy_no_pixel_left2: ENEMY_PIXEL_LIMIT := (32,31,30,29,29,29,29,30,31,31,31,32,32,33,41,41,40,40,1,1,1,1,1,1,1,1,1,1,1,1,1,1,11,12,13,1,1,41,42,42,43,43,43,43,42,47,47,47,48,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,29,29,29,29,29,29,29,29,29,29,1,1,1,1,1);
@@ -102,6 +105,7 @@ architecture bhv of vga_calc is
     constant gun_pixel_left2: GUN_PIXEL_LIMIT := (51,51,51,51,1,84,1,1,1,65,68,70,71,76,77,78,79,80,81,82,55,55,56,56,56,1,1,1,1,1);
     constant gun_pixel_right2: GUN_PIXEL_LIMIT := (53,54,54,54,0,87,0,0,0,90,90,90,90,90,88,88,88,88,87,87,60,60,60,61,61,0,0,0,0,0);
 
+    --血量、子弹数、玩家的显示位置都是固定的。Start_x表示显示位置的左上角的横坐标，
     constant hpStart_x:std_LOGIC_vector(9 downto 0) := "1000111010";
     constant hpEnd_x:std_LOGIC_vector(9 downto 0) := "1001101100";
     constant hpStart_y:std_LOGIC_vector(8 downto 0) := "000010100";
@@ -117,9 +121,11 @@ architecture bhv of vga_calc is
     constant MeStartY:std_logic_vector(8 downto 0):="101000000";
     constant MeEndY : std_logic_vector(8 downto 0):="111011111";
     
+    --代表几个变量的像素或者地址是否计算完成
     signal postOK, HpOK, enemyHPOK, gunOK, medicalOK, meOK, bulletnumOK, enemyOK, gamestartOK, 
                 gameoverOK, backgroundOK, buttonOK:std_logic;
-                
+    
+    --几个会变化的物体的位置
     signal enemy_x, medical_x, gun_x:std_logic_vector(9 downto 0);
     signal enemy_y, medical_y, gun_y:std_logic_vector(8 downto 0);
 
@@ -131,7 +137,7 @@ architecture bhv of vga_calc is
     
     signal clk25:std_logic;
 begin
-    
+    --只读sram
     rwselect <= '0';
     
     u1:vga640480 port map(
@@ -162,7 +168,7 @@ begin
 process(clk_0)
 begin
     if(s_x >= hpStart_x and s_x <= hpEnd_x and s_y >= hpStart_y and s_y <= hpEnd_y) then
-        if(CONV_INTEGER(s_x - hpStart_x) * PLAYER_HP_LIMIT <= my_hp * 50) then
+        if(CONV_INTEGER(s_x - hpStart_x) * PLAYER_HP_LIMIT <= my_hp * 50) then  --长条形状
             HpOK <= '1';
         else
             HpOK <= '0';
@@ -175,7 +181,7 @@ end process;
 process(clk_0)
 begin
     if(s_x >= BullnumStart_x and s_x <= BullnumEnd_x and s_y >= BullnumStart_y and s_y <= BullnumEnd_y) then
-        if(CONV_INTEGER(s_x - BullnumStart_x) * BULLET_NUM_LIMIT <= bullet_num * 50) then
+        if(CONV_INTEGER(s_x - BullnumStart_x) * BULLET_NUM_LIMIT <= bullet_num * 50) then  --长条形状
             BulletnumOK <= '1';
         else
             BulletnumOK <= '0';
@@ -190,6 +196,7 @@ process(clk_0)
 variable dis2:integer range 0 to 640000;
 begin
     postOK <= '0';
+    --圆形
     if(postX <= s_x and postY <= s_y) then
         dis2 := conV_INTEGER(s_x-postX)*conV_INTEGER(s_x-postX)+conV_INTEGER(s_y-postY)*conV_INTEGER(s_y-postY);
     elsif(postX <= s_x and postY > s_y) then
@@ -202,6 +209,7 @@ begin
         dis2 := 0;
     end if;
     
+    --分开火与非开火状态，改准星的颜色、大小
     if post_select = '0' then
         if(dis2 >= 100 and dis2 <= 144) then
             PostOK <= '1';
@@ -349,6 +357,7 @@ begin
     end if;
 end process;
 ----------------------------BUTTON-----------------------------------
+--开始界面的按钮
 process(clk_0)
 begin
 	buttonOK <= '0';
@@ -435,7 +444,7 @@ begin
             end if;
             --q_vga <= "0000111000";
         elsif(gunOK = '1') then
-            if(object_statuses(gun_cnt) = selected) then
+            if(object_statuses(gun_cnt) = selected) then  --分枪支、敌人是否被选中（攻击），如果选中则将红色部分赋值为111
                 temp_addr := conV_STD_LOGIC_VECTOR(conV_INTEGER(gun_x) / 2 + 
                     conV_INTEGER(gun_y) * HGUN_WIDTH, 20) + GUN_ADDR_BEGIN;
                 addr_cnt <= temp_addr;
@@ -458,6 +467,7 @@ begin
             end if;
             
         elsif(enemyOK = '1') then
+	--分为敌人自己被攻击、敌人攻击玩家的状态，敌人攻击玩家则枪口会出现火光，敌人被攻击会变红
             if(object_statuses(enemy_cnt) = selected) then
                 temp_addr := conV_STD_LOGIC_VECTOR(conV_INTEGER(enemy_x) / 2 + 
                     conV_INTEGER(enemy_y) * HENEMY_WIDTH, 20) + ENEMY_ADDR_BEGIN;
